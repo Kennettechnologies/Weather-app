1482b9a32cdfed51db18fb10e76e8253c0f72aeb
Kennettechnologies <kevinonyangowanga@gmail.com>
2025-06-04 00:03:29 +0300

    Add debug logging configuration

--- weather_project/settings.py
+++ weather_project/settings.py
@@ -87,6 +87,32 @@ DATABASES = {
     )
 }
 
+# Logging configuration
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'INFO',
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+    },
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
