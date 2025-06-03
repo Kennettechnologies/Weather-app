5e1952f6dffe5d6ef34ff0fced63d0d076b692d7
Kennettechnologies <kevinonyangowanga@gmail.com>
2025-06-03 23:24:11 +0300

    Update configuration for Vercel deployment

--- weather_project/wsgi.py
+++ weather_project/wsgi.py
@@ -1,7 +1,7 @@
 """
 WSGI config for weather_project project.
 
-It exposes the WSGI callable as a module-level variable named ``handler``.
+It exposes the WSGI callable as a module-level variable named ``app``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
@@ -12,8 +12,8 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "weather_project.settings")
 
-# This is the variable that Vercel looks for
-handler = get_wsgi_application()
+# Vercel needs this to be named 'app'
+app = get_wsgi_application()
 
 # For local development
-application = handler
+application = app
