5b8ef1ddfe844e49d762369062bb8d0b1c642c23
Kennettechnologies <kevinonyangowanga@gmail.com>
2025-06-03 23:14:02 +0300

    Fix handler exposure in index.py for Vercel

--- api/index.py
+++ api/index.py
@@ -1,4 +1,10 @@
-from weather_project.wsgi import handler
+import os
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "weather_project.settings")
+
+# This is the variable that Vercel looks for
+handler = get_wsgi_application()
 
 def lambda_handler(event, context):
     return handler(event, context) 
