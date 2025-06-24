59dfc2c364c948a0b160c5ce5a921955df5e8ef0
Kennettechnologies <kevinonyangowanga@gmail.com>
2025-06-24 09:18:46 +0300

    Resolve merge conflicts

--- build_files.sh
+++ build_files.sh
@@ -6,8 +6,5 @@ python3 -m pip install -r requirements.txt
 # Collect static files
 python3 manage.py collectstatic --noinput
 
-# Make migrations
-python3 manage.py makemigrations
-
 # Apply migrations
-python3 manage.py migrate 
+python3 manage.py migrate --noinput 
